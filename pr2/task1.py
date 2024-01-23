6f4d1be4d987c1b094935c67aacc081097d8585f
ANASTASIA <156909623+nastyatrybitsyna@users.noreply.github.com>
2024-01-23 20:39:53 +0200

    task1.py

--- pr2/task1.py
+++ pr2/task1.py
@@ -3,8 +3,3 @@ if __name__ == "__main__":
     while number <= 10:
         print(number)
         number += 1
-        
-#цикл for
-if __name__ == "__main__":
-    for number in range(1, 11):
-        print(number)
